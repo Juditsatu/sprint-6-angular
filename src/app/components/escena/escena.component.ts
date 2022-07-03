7181199eb12c9a943e9854f30639dcafc6c32dc6
Judit Santos <judit.sant@gmail.com>
2022-07-03 11:35:33 +0200

    lvl 3

--- src/app/components/escena/escena.component.ts
+++ src/app/components/escena/escena.component.ts
@@ -12,26 +12,20 @@ export class EscenaComponent implements OnInit {
   @Input() public stories: Story[] = [];
 
   currentSentence: number = 0;
-  currentImg: number = 0;
-  
 
   public prev() {
     if (this.currentSentence == 0) {
       this.currentSentence = this.stories.length - 1;
-      this.currentImg = this.stories.length - 1;
     } else {
       this.currentSentence--;
-      this.currentImg--;
     }
   }
   
   public next() {
     if (this.currentSentence == this.stories.length - 1) {
       this.currentSentence = 0;
-      this.currentImg = 0;
     } else {
       this.currentSentence++
-      this.currentImg++
     }
   }
   
